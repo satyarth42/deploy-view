562bb10417de865c07a2bf30ec9e1d05c8c0f7c7
satyarth.ojha <satyarth.ojha@grabpay.com>
2020-07-15 20:23:01 +0530

    added prefix in url

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      name:""
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({name:e.target.value})
+  }
+
+  submitName = () =>{
+    if(this.state.name){
+      axios.post('/api/add', {
+        name:this.state.name
+      })
+      .then(response=> {
+        console.log(response.data);
+        this.setState({name:""})
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  }
+
+  fetchList = () => {
+    axios.get('/api/fetch')
+    .then(response=> {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  render(){
+    return (
+      <div style={{margin:"300px"}}>
+        <input type="text" value={this.state.name} onChange={this.handleChange}/><br/>
+        <button onClick={this.submitName}>Insert</button>
+        <button onClick={this.fetchList}>Get List</button>
+      </div>
+  );}
 }
 
 export default App;
